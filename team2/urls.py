dedf44a5985c3b9c3c555e2243e66072282c3a58
Armin <armin.hamedazimi@cafebazaar.ir>
2026-02-11 16:07:23 +0330

    Add API views and URL patterns for article, version, and vote operations to team2

--- team2/urls.py
+++ team2/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.base),
     path("ping/", views.ping),
+    path("api/articles/create/", views.create_article, name="team2-create-article"),
+    path("api/articles/<str:article_name>/", views.get_article, name="team2-get-article"),
+    path("api/versions/create/", views.create_version_from_version, name="team2-create-version"),
+    path("api/versions/<str:version_name>/", views.get_version, name="team2-get-version"),
+    path("api/vote/", views.vote, name="team2-vote"),
 ]
