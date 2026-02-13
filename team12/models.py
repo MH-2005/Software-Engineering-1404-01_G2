34e1b4696bd05474c6ae1bd93034eecef7f4743c
mehrabear <mmehrad345@gmail.com>
2026-02-13 10:50:57 +0330

    Added Region to models

--- team12/models.py
+++ team12/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Region(models.Model):
+    region_id = models.CharField(max_length=100, unique=True)
+    region_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.region_name
+
+
 class Place(models.Model):
     STYLE_CHOICES = [
         ('SOLO', 'Solo'), ('COUPLE', 'Couple'), ('FAMILY', 'Family'),
@@ -14,13 +22,15 @@ class Place(models.Model):
 
     place_id = models.CharField(max_length=100, unique=True)
     place_name = models.CharField(max_length=255)
-    region_id = models.CharField(max_length=100)
-    region_name = models.CharField(max_length=255)
     budget_level = models.CharField(max_length=20, choices=BUDGET_CHOICES)
     travel_style = models.CharField(max_length=20, choices=STYLE_CHOICES)
     duration = models.IntegerField()
     season = models.CharField(max_length=20, choices=SEASON_CHOICES)
     image_url = models.URLField(blank=True, null=True)
-    
+    season = models.CharField(max_length=20, choices=SEASON_CHOICES)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='places', null=True)
+
     def __str__(self):
         return self.place_name
+
+
