5a514d264f96b6d819b9fd6ba719f5d69236825c
Mohammad-Reza Hasanzadeh <104139539+MH-2005@users.noreply.github.com>
2026-02-13 12:05:07 +0330

    changes in urls

--- team12/urls.py
+++ team12/urls.py
@@ -1,18 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-
-TEAM_PREFIX = "team12/"
 
 urlpatterns = [
     path("", views.base, name="base"),
     path("ping/", views.ping, name="ping"),
+    
+    # POST /team12/recommend/places/score
+    path("recommend/places/score/", views.ScoreCandidatePlacesView.as_view(), name="score-places"),
 
-    # 1. POST /team12/recommend/places/score
-    path("places/score", views.ScoreCandidatePlacesView.as_view(), name="score-candidate-places"),
+    # GET /team12/recommend/regions
+    path("recommend/regions/", views.SuggestRegionsView.as_view(), name="suggest-regions"),
 
-    # 2. GET /team12/recommend/regions
-    path("regions", views.SuggestRegionsView.as_view(), name="suggest-regions"),
-
-    # 3. GET /team12/recommend/regions/{region_id}/places
-    path("regions/<str:region_id>/places", views.SuggestPlacesInRegionView.as_view(), name="suggest-places-in-region"),
+    # GET /team12/recommend/regions/{region_id}/places
+    path("recommend/regions/<str:region_id>/places/", views.SuggestPlacesInRegionView.as_view(),
+         name="suggest-places-in-region"),
 ]
